94ecd28b586ada803862018f901ce9fac1ab6d49
f-mohrechi <f.mohrechi78@gmail.com>
2023-10-20 12:29:24 +0330

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="github-api">
+      <BrowserRouter basename="/github-api">
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Username />} path="/:username" />
