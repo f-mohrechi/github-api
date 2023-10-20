5a05928caf830020a464403821914366f9ba7c65
f-mohrechi <f.mohrechi78@gmail.com>
2023-10-20 12:27:30 +0330

    add base name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="github-api">
         <Routes>
           <Route element={<Home />} path="/" />
           <Route element={<Username />} path="/:username" />
